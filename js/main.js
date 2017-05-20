97ab8bf88dfc6b105548b61a0049a49c6d691f07
zhouchao <zhouchao@daishu360.com>
2017-05-20 20:21:08 +0800

    自定义图标

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var subpage_style = {
+	top: '0px',
+	bottom: '51px'
+}
+
+mui.plusReady(function() {
+	mui.init({
+		swipeBack: false
+	});
+
+	//选项卡点击事件
+	mui('.mui-bar-tab').on('tap', 'a', function(e) {
+		var url = this.getAttribute('href');
+		if(url == "") {
+			return;
+		}
+
+		var pagetype = this.getAttribute('pagetype');
+		if(pagetype == "main") {
+			openwindow(url, subpage_style);
+		} else {
+			openwindow(url);
+		}
+	});
+});
+
+function openwindow(url, styles) {
+	mui.openWindow({
+		url: url,
+		createNew: true,
+		show: {
+			autoShow: true
+		},
+		waiting: {
+			autoShow: true
+		},
+		styles: styles
+	});
+}
